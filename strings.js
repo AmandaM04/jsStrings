a5f674081b910cd50d21b07effd5b454a356cb81
Amanda Mitchell <wreedamitchell@gmail.com>
2018-02-26 21:14:12 -0600

    challenge 4 completed

--- strings.js
+++ strings.js
@@ -2,3 +2,24 @@
 
 var numOfSandwiches = 4*2;
 console.log("numOfSandwiches", numOfSandwiches);
+
+// Challenge #2
+
+var name = "Amanda"
+var string = "Hello, " + name +" how are you doing today?"
+console.log("string", string);
+
+// Challenge #3
+
+var DNA = "GCAT"
+var RNA =  DNA.replace('T', 'U');
+console.log("RNA:", RNA);
+
+// Challenge #4
+
+var animal = "ALLigator"
+if(animal.toLowerCase() === "alligator"){
+    console.log("small")
+}else {
+    console.log("wide")
+}
